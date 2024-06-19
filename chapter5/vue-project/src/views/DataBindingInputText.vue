8309e1febcb7edc0123b3a14bd5fa3c6fed638f9
hello-cjlee-s-world <153203021+hello-cjlee-s-world@users.noreply.github.com>
2024-06-19 23:55:19 +0900

    chapter5 진행중
    --chapter5 컴포넌트 Basic
    	5.1 컴포넌트란?
    	5.2 컴포넌트 구조 이해하기
    	5.3 데이터 바인딩
    		--> DataBinding.vue 파일 생성 후, index.js, App.vue에 추가 후 npm run serve 하니 하기 에러 발생
    			ERROR in [eslint]
    			C:\Users\cndwn\Documents\GitHub\vue_js_one_week\chapter5\vue-project\src\views\DataBinding.vue
    			6:1 error Expected indentation of 2 spaces but found 4 indent
    			6:9 error Missing space before function parentheses space-before-function-paren
    			7:1 error Expected indentation of 4 spaces but found 8 indent
    			8:1 error Expected indentation of 6 spaces but found 12 indent
    			9:1 error Expected indentation of 4 spaces but found 8 indent
    			10:1 error Expected indentation of 2 spaces but found 4 indent
    			12:10 error Newline required at end of file but not found eol-last
    			--> ESLint 에러는 코드 스타일 가이드라인을 위반했을 때 발생, 바꾸라는대로 바꾸니 에러 사라짐

---
+++ chapter5/vue-project/src/views/DataBindingInputText.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <input type="text" v-model="valueModel">
+    </div>
+    <div>text에서 글자를 바꾸면 여기서 이렇게! {{valueModel}} 바뀝니다.</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      valueModel: 'South Korea'
+    }
+  }
+}
+</script>
