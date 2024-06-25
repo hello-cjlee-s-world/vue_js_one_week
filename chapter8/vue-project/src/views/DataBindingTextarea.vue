09981e32e81d62b954145ad54516f40a999e03e2
hello-cjlee-s-world <153203021+hello-cjlee-s-world@users.noreply.github.com>
2024-06-25 14:51:12 +0900

    chapter8
    --chapter8 컴포넌트 심화 학습
    	8.1 컴포넌트 안에 다른 컴포넌트 사용하기
    	8.2 Slot
    	8.3 Provide/Inject

---
+++ chapter8/vue-project/src/views/DataBindingTextarea.vue
@@ -0,0 +1,12 @@
+<template>
+    <textarea v-model="message"></textarea>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      message: '여러 줄을 입력할 수 있는 textarea입니다.'
+    }
+  }
+}
+</script>
---
+++ chapter8/vue-project/src/views/eventClick.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button type="button" @click="increaseCounter">Add 1</button>
+    <button type="button" @click="setCount(7)">Set 7</button>
+    <button type="button" @click="one">Set 1</button>
+    <button type="button" @click="two">Set 2</button>
+    <p>The counter is {{ counter }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter () {
+      this.counter = this.counter + 1
+      console.log(this.counter)
+    },
+    setCount (counter) {
+      this.counter = counter
+    },
+    one () {
+      this.counter = 1
+      alert(1)
+    },
+    two () {
+      this.counter = 2
+      alert(2)
+    }
+  }
+}
+</script>
